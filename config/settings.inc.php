b6970a3bcfb02910781181401c310cb3de3e6545
Tomas Jacik <foxycode@sunfox.cz>
2016-10-28 15:18:41 +0200

    Zmeny provedene instalaci, vcetne pridani ceskeho jazyka

---
+++ config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'workshop');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', '');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'idC3P3t2PTRD386aisStxtkvzhpRR1WpKF7PPgVtLnrHFzAHHnRG5jjf');
+define('_COOKIE_IV_', 'movY6PHd');
+define('_PS_CREATION_DATE_', '2016-10-28');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.8');
+define('_RIJNDAEL_KEY_', 'vYb47pvShYy1hEcpEJeywkDXvMfsLqvX');
+define('_RIJNDAEL_IV_', 'k57u3FmgpFPh0OL5CjxFzQ==');
